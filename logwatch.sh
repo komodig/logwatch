8fbd9190b921477fe9180962a3df8269cafd8861
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2022-01-28 14:19:34 +0100

    some cleanup

--- logwatch.sh
+++ logwatch.sh
@@ -24,32 +24,7 @@ else
         exit
 fi
 
-logger "+++ logwatch +++ run"
-
 python logwatch.py
 
-#python $plogparse -f "/etc/hosts.deny" -s "ALL.*" -l $hosts_list
-#    python $plogparse -f $iptables_rules -s "REJECT.*all.*icmp-port-unreachable" -l $iptables_list
-#        logger "+++ logwatch +++ blocking ip: $doom_ip"
-#        $iptables -t filter -I INPUT -s $doom_ip -j REJECT
-
-#    python $plogparse -f $iptables_rules -s "DROP\ *all.*\d{1,3}\.\d{1,3}\.\d{1,3}\.0/24" -l $subnet_rules
-
-#            $iptables -t filter -I INPUT -s "$subnet/255.255.255.0" -j DROP
-#            log_msg "+++ logwatch +++ blocking subnet: $subnet"
-#    for ban_ip in `cat $hosts_list`; do
-#        echo "# added by logwatch at `date`" >> $tmpfile
-#        echo "ALL:$ban_ip" >> $tmpfile
-
-#        curl -iX POST "$api_url" \
-#        -H "Accept: application/json" \
-#        -H "Referer: rs-test" \
-#        -H "Content-Type: application/json" \
-#        -H "Authorization: Token $api_key" \
-#        -d "{\"ip\": \"$ban_ip\", \"origin\": \"\"}"
-
-
-#        cat $tmpfile | mail -s "logwatch reports: new blacklisted clients @ $host" $webmaster
-
-echo "exit down venv"
+echo "shutting down venv"
 deactivate
