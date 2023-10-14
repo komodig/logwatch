2b1705a523a05e21c7cfa9cccb7a090fc00b9b98
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2023-10-14 12:02:23 +0200

    grep hosts reviewed

--- logwatch.py
+++ logwatch.py
@@ -33,8 +33,7 @@ def raw_input_generator(type_: int, path: str):
 def grep_hosts(type_: int, hosts: list, log: str, pattern: str, limit: int) -> list:
     ip_hits = {}
     err_pattern = re.compile(pattern)
-    ip_pattern = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
-    detected_hosts = []
+    ip_pattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 
     for line in raw_input_generator(type_, log):
         if err_pattern.match(line):
@@ -42,16 +41,13 @@ def grep_hosts(type_: int, hosts: list, log: str, pattern: str, limit: int) -> l
                 ipaddr = hit.group()
                 break    # just 1st ip if there are more in one line
 
-            if ipaddr not in hosts and ipaddr not in detected_hosts:
+            if ipaddr not in hosts:
                 if ipaddr in ip_hits.keys():
                     ip_hits[ipaddr] += 1
                 else:
                     ip_hits[ipaddr] = 1
 
-                if ip_hits[ipaddr] >= limit:
-                    detected_hosts.append(ipaddr)
-
-    return detected_hosts
+    return [ipaddr for ipaddr in ip_hits.keys() if ip_hits[ipaddr] >= limit]
 
 
 def read_config(cfile: str) -> dict:
@@ -73,7 +69,7 @@ def read_hosts_file(hfile: str) -> list:
 
 
 def write_hosts_file(hfile: str, hosts: list):
-    syslog.syslog(syslog.LOG_INFO, f"saving {str(len(hosts))} hosts")
+    syslog.syslog(syslog.LOG_INFO, f'saving {str(len(hosts))} hosts')
     with open(hfile, 'w') as lwhosts:
         json.dump({"hosts": hosts}, lwhosts)
 
@@ -82,28 +78,28 @@ def read_hosts_api(url: str) -> list:
     syslog.syslog(syslog.LOG_INFO, 'requesting hosts from api')
     resp = requests.get(url)
     if resp.status_code != 200:
-        syslog.syslog(syslog.LOG_INFO, f"API retrieve failed: {resp.status_code}")
+        syslog.syslog(syslog.LOG_INFO, f'API retrieve failed: {resp.status_code}')
         return []
 
     hosts = [host['ip'] for host in json.loads(resp.content)]
-    syslog.syslog(syslog.LOG_INFO, str(len(hosts)) + " hosts from api")
+    syslog.syslog(syslog.LOG_INFO, str(len(hosts)) + ' hosts from api')
     return hosts
 
 
 def sys_ban_ip(iptables: str, host: str, conf: dict):
     if host in conf['whitelist']:
-        syslog.syslog(syslog.LOG_INFO, f"skip rule: {host} in whitelist")
+        syslog.syslog(syslog.LOG_INFO, f'skip rule: {host} in whitelist')
         return
 
-    syslog.syslog(syslog.LOG_INFO, "insert rule: reject access: " + host)
+    syslog.syslog(syslog.LOG_INFO, 'insert rule: reject access: ' + host)
     try:
         _ = subprocess.check_output([iptables, '-t', 'filter', '-I', 'INPUT', '-s', host, '-j', 'REJECT'])
     except subprocess.CalledProcessError as grepexc:
-        syslog.syslog(syslog.LOG_INFO, "subprocess error code: ", grepexc.returncode, grepexc.output)
+        syslog.syslog(syslog.LOG_INFO, 'subprocess error code: ', grepexc.returncode, grepexc.output)
 
 
 def load_from_iptables(iptables: str) -> list:
-    pattern = "REJECT.*all.*icmp-port-unreachable"
+    pattern = 'REJECT.*all.*icmp-port-unreachable'
     blacklisted = grep_hosts(INPUT_TYPE.IPTABLES, [], iptables, pattern, 1)
     syslog.syslog(syslog.LOG_INFO, str(len(blacklisted)) + ' blacklisted in iptables')
     return blacklisted
@@ -111,7 +107,7 @@ def load_from_iptables(iptables: str) -> list:
 
 def submit_to_blacklistAPI(conf: dict, attacker: str, directive: str):
     if attacker in conf['whitelist']:
-        syslog.syslog(syslog.LOG_INFO, f"API submit canelled: {attacker} in whitelist")
+        syslog.syslog(syslog.LOG_INFO, f'API submit canelled: {attacker} in whitelist')
         return
 
     headers = {"Content-Type": "application/json",
@@ -121,9 +117,9 @@ def submit_to_blacklistAPI(conf: dict, attacker: str, directive: str):
     payload = "{\"ip\": \"%s\", \"origin\": \"\", \"directive\": \"%s/%s\"}" % (attacker, conf['domain'], directive)
     resp = requests.post(conf['api']['url'], data=payload, headers=headers)
     if resp.status_code != 201:
-        syslog.syslog(syslog.LOG_INFO, f"API submit failed: {resp.status_code} : {attacker}")
+        syslog.syslog(syslog.LOG_INFO, f'API submit failed: {resp.status_code} : {attacker}')
     else:
-        syslog.syslog(syslog.LOG_INFO, f"API submit successful: {attacker}")
+        syslog.syslog(syslog.LOG_INFO, f'API submit successful: {attacker}')
 
 
 if __name__ == '__main__':
@@ -149,7 +145,7 @@ if __name__ == '__main__':
         if not len(detected_hosts):
             continue
 
-        syslog.syslog(syslog.LOG_INFO, f"directive: {dir} detected {len(detected_hosts)} hosts")
+        syslog.syslog(syslog.LOG_INFO, f'directive: {dir} detected {len(detected_hosts)} hosts')
         for attacker in detected_hosts:
             submit_to_blacklistAPI(conf, attacker, dir)
 
@@ -160,10 +156,10 @@ if __name__ == '__main__':
     if len(new_hosts_detected):
         # just use the event to check updates from API
         new_api_hosts = [ ah for ah in api_hosts if ah not in hosts ]
-        syslog.syslog(syslog.LOG_INFO, f"received {str(len(new_api_hosts))} from api")
+        syslog.syslog(syslog.LOG_INFO, f'received {str(len(new_api_hosts))} from api')
         hosts += new_api_hosts
     else:
-        syslog.syslog(syslog.LOG_INFO, "no suspicious hosts detected")
+        syslog.syslog(syslog.LOG_INFO, 'no suspicious hosts detected')
         api_hosts = []
 
     for attacker in hosts:
